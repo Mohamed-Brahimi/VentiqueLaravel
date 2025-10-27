31f06f6388ea81e987fa5bd88bbf3668620a9907
Adam Mahmoudi <2396547@cmontmorency.qc.ca>
2025-10-27 11:30:40 -0400

    v0.2.1.4: Apropos et traductions

--- resources/views/apropos.blade.php
+++ resources/views/apropos.blade.php
@@ -10,6 +10,19 @@
             <p> <B>@lang("apropos_texte")</B></p>
             <h2> @lang("apropos_texte_image")</h2>
 
+            <h2>@lang("apropos_noms")</h2>
+            <ul>
+                <li>@lang("apropos_cours1")</li>
+                <li>@lang("apropos_cours2")</li>
+            </ul>
+
+            <ul>
+                <li>@lang("apropos_interactions1")</li>
+                <li>@lang("apropos_interactions2")</li>
+                <li>@lang("apropos_interactions3")</li>
+                
+            </ul>
+            
 
         </div>
 @endsection
